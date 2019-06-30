781283fa463790bfe9fa10d4b7629b6eb5cf95e8
IanRicGD <ianricardo2011@hotmail.com>
2019-06-30 17:40:21 -0500

    Version 1.0

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.parallax').parallax();
+	$('.datepicker').datepicker();
+	$('.dropdown-trigger').dropdown();
+	$('.collapsible').collapsible();
+	$('.slider').slider();
+	$('select').formSelect();
+});
+
+$('.carousel.carousel-slider').carousel({
+	fullWidth: true,
+	indicators: true
+});
